a6cba7511f461771bc7e4d5b940c2dd4725177ba
Darko <darkoangelovski666@gmail.com>
2025-06-16 23:20:21 +0200

    added use case to check if user is logged in

--- app/src/main/java/com/solo/studybudy/MainViewModel.kt
+++ app/src/main/java/com/solo/studybudy/MainViewModel.kt
@@ -1,23 +1,38 @@
 package com.solo.studybudy
 
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.FirebaseAuth
+import androidx.lifecycle.viewModelScope
+import com.solo.core.CustomResult
+import com.solo.core.domain.useCase.IsUserLoggedInUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val firebaseAuth: FirebaseAuth
+    private val isUserLoggedInUseCase: IsUserLoggedInUseCase
 ) : ViewModel() {
 
-    private val _isUserLoggedIn = MutableStateFlow(false)
+    private val _isUserLoggedIn = MutableStateFlow<Boolean?>(null)
     val isUserLoggedIn = _isUserLoggedIn.asStateFlow()
 
     init {
-        _isUserLoggedIn.value = firebaseAuth.currentUser?.uid != null
-        Timber.d("Current user id is ${firebaseAuth.currentUser?.uid}")
+        viewModelScope.launch(Dispatchers.IO) {
+            when (val result = isUserLoggedInUseCase()) {
+                is CustomResult.Success -> {
+                    Timber.d("User is logged in: ${result.data}")
+                    _isUserLoggedIn.value = result.data
+                }
+
+                is CustomResult.Failure -> {
+                    _isUserLoggedIn.value = false
+                    Timber.e(result.throwable, "Login state check failed: ${result.message}")
+                }
+            }
+        }
     }
 }
--- core/src/main/java/com/solo/core/domain/useCase/BaseUseCaseNoParams.kt
+++ core/src/main/java/com/solo/core/domain/useCase/BaseUseCaseNoParams.kt
@@ -1,5 +1,7 @@
 package com.solo.core.domain.useCase
 
+import com.solo.core.CustomResult
+
 abstract class BaseUseCaseNoParams<out Type> where Type : Any {
-    abstract fun invoke(): Type
+    abstract suspend operator fun invoke(): CustomResult<Type>
 }
